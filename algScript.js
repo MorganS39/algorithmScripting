38ba9fdc475bc4ce61cef49f835ca16d53b063a8
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 02:26:29 -1000

    Truncate a string

--- algScript.js
+++ algScript.js
@@ -78,4 +78,14 @@ function repeatStringNumTimes(str, num) {
 repeatStringNumTimes("abc", 3);
 
 // Truncate a string
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
 
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+//
