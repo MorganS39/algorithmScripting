83529313fb0da5289a9d5d6888feeda3f636a2b2
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-02 17:14:56 -1000

    Reverse a string test (failed)

--- algScript.js
+++ algScript.js
@@ -1,9 +1,19 @@
-// Convert celsius to fahrenheit test
+// Convert celsius to fahrenheit test (passed)
 function convertToF(celsius) {
     let fahrenheit = (celsius * 9/5) + 32;
     return fahrenheit;
 }
 
 convertToF(30);
+
+// Reverse a string test (failed)
+function reverseString(str) {
+    for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+}
+
+reverseString("hello");
 
 //
