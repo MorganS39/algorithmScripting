df5273b6fe4666879f0cad122a5c2e97b68a72a4
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 02:21:20 -1000

    Repeat a string

--- algScript.js
+++ algScript.js
@@ -64,4 +64,18 @@ function confirmEnding(str, target) {
 
 confirmEnding("Bastian", "n");
 
+// Repeat a string
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  } else if (num === 1) {
+    return str;
+  } else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+repeatStringNumTimes("abc", 3);
+
+// Truncate a string
 
