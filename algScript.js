435094116edfa3d56600399853810bbe9840bf76
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 03:08:34 -1000

    Mutations

--- algScript.js
+++ algScript.js
@@ -150,4 +150,17 @@ function getIndexToIns(arr, num) {
 
 getIndexToIns([40, 60], 50);
 
-//
+// Mutations
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (var i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0)
+    return false;
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
+
+// 
