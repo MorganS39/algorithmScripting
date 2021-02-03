58a80a655a290f8003d9821ebe0133d7da4875e3
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-03 03:12:26 -1000

    Chunky Monkey

--- algScript.js
+++ algScript.js
@@ -163,4 +163,13 @@ function mutation(arr) {
 
 mutation(["hello", "hey"]);
 
-// 
+// Chunky Monkey
+function chunkArrayInGroups(arr, size) {
+  var arr2 = [];
+  for (var i = 0; i < arr.length; i += size) {
+    arr2.push(arr.slice(i, i + size));
+  }
+  return arr2;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
